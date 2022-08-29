b726389735147a062aa7c7b5c1c9814d641ed085
Mateo Pineiro <mateopineiro2001@gmail.com>
2022-08-29 13:14:46 -0300

    asd

--- proyecto-ejemplo/src/App.jsx
+++ proyecto-ejemplo/src/App.jsx
@@ -7,6 +7,7 @@ import ListadoPacientes from './components/ListadoPacientes';
 
 function App() {
   const [pacientes, setPacientes] = useState([])
+  const [paciente, setPaciente] = useState({})
 
   const imprimir2mas2 = () => {
     console.log(2+2)
@@ -14,10 +15,20 @@ function App() {
 
   return (
     <div className="container mx-auto mt-5 w-4/5">     {/* El return de un componente jsx debe ser de solo un contenedor. Puede ser: <></> */}
-      <Header numeros={1} fn={imprimir2mas2} isAdmin={false} />
+      <Header 
+        numeros={1} 
+        fn={imprimir2mas2} 
+        isAdmin={false} />
       <div className="mt-12 flex flex-col md:flex-row">
-        <Formulario pacientes={pacientes} setPacientes={setPacientes}/>
-        <ListadoPacientes pacientes={pacientes} /> 
+        <Formulario 
+          pacientes={pacientes} 
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}/>
+          
+        <ListadoPacientes 
+          pacientes={pacientes} 
+          setPaciente={setPaciente} /> 
       </div>
 
     </div>
